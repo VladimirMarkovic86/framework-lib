a230eff1795087184aef614000ddf024ea783f88
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:44:55 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,15 +1,18 @@
-(defproject org.vladimir/framework-lib "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/framework-lib "0.1.0"
   :description "Framework library"
-  :url "http://gitlab:1610/VladimirMarkovic86/framework-lib"
+  :url "https://github.com/VladimirMarkovic86/framework-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.vladimir/ajax-lib "0.1.0"]
-                 [org.vladimir/htmlcss-lib "0.1.0"]
-                 [org.vladimir/js-lib "0.1.0"]
-                 [org.vladimir/utils-lib "0.1.0"]
-                 [org.vladimir/language-lib "0.1.0"]
-                 [org.vladimir/common-middle "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/js-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.1.0"]
                  ]
+
+  :min-lein-version "2.0.0"
+  
   :source-paths ["src/cljs"])
 
