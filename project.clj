c09227d5005e5d89694cd83da4ee43dec7e13a5d
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-11-23 16:48:32 +0100

    Implemented httponly session cookie.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.7"
+(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.8"
   :description "Framework library"
   :url "https://github.com/VladimirMarkovic86/framework-lib"
   :license {:name "Eclipse Public License"
@@ -8,8 +8,8 @@
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.0"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.0"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.1"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.2"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.1"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.3"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.2"]
                  ]
 
   :min-lein-version "2.0.0"
