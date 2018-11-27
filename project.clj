3d07678084f2b69601bcf8a8be72334c2c08d679
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-11-27 16:03:10 +0100

    Task number: 3 Implemented 304 not modified scenario for files of size less then 64 KB.

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.8"
+(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.9"
   :description "Framework library"
   :url "https://github.com/VladimirMarkovic86/framework-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.1"]
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.0"]
-                 [org.clojars.vladimirmarkovic86/js-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/js-lib "0.1.1"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.1"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.3"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.4"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.2"]
                  ]
 
