2c05c683b85e7ef52c7a092fc66618cc75e05022
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-01-23 17:18:37 +0100

    Task 51: Accepted language bug fixed.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.10"
+(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.11"
   :description "Framework library"
   :url "https://github.com/VladimirMarkovic86/framework-lib"
   :license {:name "Eclipse Public License"
@@ -8,9 +8,9 @@
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.1"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.2"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.2"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.5"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.6"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.2"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.1"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.2"]
                  ]
 
   :min-lein-version "2.0.0"
