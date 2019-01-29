72d2ffdf8583e0938c76197b6429c46f3fdc47b0
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-01-29 17:54:43 +0100

    Task 30: Implemented chat on common (client, middle, server) projects

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.12"
+(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.13"
   :description "Framework library"
   :url "https://github.com/VladimirMarkovic86/framework-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.2"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.3"]
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.1"]
-                 [org.clojars.vladimirmarkovic86/js-lib "0.1.2"]
+                 [org.clojars.vladimirmarkovic86/js-lib "0.1.3"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.2"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.6"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.2"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.2"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.7"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.3"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.3"]
                  ]
 
   :min-lein-version "2.0.0"
