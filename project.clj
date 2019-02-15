639254ccc1668a96ceee04918c5d332ed3fdbcb8
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-02-15 11:02:46 +0100

    Task 54: Fixed po and ocr

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.16"
+(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.17"
   :description "Framework library"
   :url "https://github.com/VladimirMarkovic86/framework-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.4"]
-                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.2"]
-                 [org.clojars.vladimirmarkovic86/js-lib "0.1.4"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.5"]
+                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.3"]
+                 [org.clojars.vladimirmarkovic86/js-lib "0.1.5"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.3"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.10"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.4"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.6"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.11"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.5"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.7"]
                  ]
 
   :min-lein-version "2.0.0"
