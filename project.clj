5f34827284df456bbb73c3cd8d8636ce64049ec3
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-03-29 16:17:27 +0100

    Task 63: Implemented tests for session-lib.core namespace.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.26"
+(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.27"
   :description "Framework library"
   :url "https://github.com/VladimirMarkovic86/framework-lib"
   :license {:name "Eclipse Public License"
@@ -8,9 +8,9 @@
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.3"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.8"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.7"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.20"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.21"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.5"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.16"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.17"]
                  ]
 
   :min-lein-version "2.0.0"
