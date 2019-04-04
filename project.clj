c4bfc59aaaa2ba1bd292ab2e09a6062a8ebf0215
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-04-04 18:00:03 +0200

    Task63: Implemented server-lib.core namespace tests and fixed some bugs.

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.27"
+(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.28"
   :description "Framework library"
   :url "https://github.com/VladimirMarkovic86/framework-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.8"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.9"]
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.3"]
-                 [org.clojars.vladimirmarkovic86/js-lib "0.1.8"]
+                 [org.clojars.vladimirmarkovic86/js-lib "0.1.9"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.7"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.21"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.22"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.5"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.17"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.18"]
                  ]
 
   :min-lein-version "2.0.0"
