538803ecfec04104d40ba1e4f74196653fed50cf
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-05-09 17:28:31 +0200

    Task 66: Implemented charts in svg-lib and used it in sample-client.

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.29"
+(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.30"
   :description "Framework library"
   :url "https://github.com/VladimirMarkovic86/framework-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.9"]
-                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.3"]
-                 [org.clojars.vladimirmarkovic86/js-lib "0.1.9"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.7"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.23"]
+                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.4"]
+                 [org.clojars.vladimirmarkovic86/js-lib "0.1.10"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.8"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.24"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.6"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.19"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.20"]
                  ]
 
   :min-lein-version "2.0.0"
