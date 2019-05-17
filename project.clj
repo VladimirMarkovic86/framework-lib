6c8f61c7c6a8af503c077df7fd25790d885637f0
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-05-17 17:45:16 +0200

    Task 63: Implemented tests for htmlcss-lib.core namespace.

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.31"
+(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.32"
   :description "Framework library"
   :url "https://github.com/VladimirMarkovic86/framework-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.9"]
-                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.5"]
-                 [org.clojars.vladimirmarkovic86/js-lib "0.1.11"]
+                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.6"]
+                 [org.clojars.vladimirmarkovic86/js-lib "0.1.12"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.9"]
                  [org.clojars.vladimirmarkovic86/language-lib "0.2.25"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.6"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.21"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.22"]
                  ]
 
   :min-lein-version "2.0.0"
