806dace19d0e5667482c7cacb04ec5d417c0268f
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-12 15:49:53 +0200

    Task 88: Implemented droid-server project, it is as library to be used with AndroidServer android java project.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.43"
+(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.44"
   :description "Framework library"
   :url "https://github.com/VladimirMarkovic86/framework-lib"
   :license {:name "Eclipse Public License"
@@ -7,10 +7,10 @@
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.11"]
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.6"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.16"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.9"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.31"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.10"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.32"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.9"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.32"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.33"]
                  ]
 
   :min-lein-version "2.0.0"
