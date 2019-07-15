50263c6e7b93548262ff437794f256e788956d6e
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 18:00:58 +0200

    Task 88: Rolled back README.md and LICENSE files to appear in jar files.

--- project.clj
+++ project.clj
@@ -1,25 +1,23 @@
-(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.48"
+(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.49"
   :description "Framework library"
   :url "https://github.com/VladimirMarkovic86/framework-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.13"]
-                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.8"]
-                 [org.clojars.vladimirmarkovic86/js-lib "0.1.18"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.12"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.35"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.10"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.37"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.14"]
+                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.9"]
+                 [org.clojars.vladimirmarkovic86/js-lib "0.1.19"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.13"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.36"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.11"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.38"]
                  ]
 
   :min-lein-version "2.0.0"
   
   :source-paths ["src/cljs"]
   
-  :jar-exclusions [#"^public/"
-                   #"README.md$"
-                   #"LICENSE$"]
+  :jar-exclusions [#"^public/"]
 
   :plugins [[lein-cljsbuild  "1.1.7"]
             [lein-doo "0.1.11"]
