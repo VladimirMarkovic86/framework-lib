e2966666b24ac072dd585467e9ee0b03d7d5d45c
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 13:17:10 +0200

    Task 88: Excluded public folder from some more clojurescripts projects packing jar file.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.46"
+(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.47"
   :description "Framework library"
   :url "https://github.com/VladimirMarkovic86/framework-lib"
   :license {:name "Eclipse Public License"
@@ -10,7 +10,7 @@
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.11"]
                  [org.clojars.vladimirmarkovic86/language-lib "0.2.34"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.9"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.35"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.36"]
                  ]
 
   :min-lein-version "2.0.0"
