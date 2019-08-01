e019ac52e036eb5ddffb70352a57c97b88afbe06
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-08-01 13:17:59 +0200

    Task 95: Refactored the way routing works.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.50"
+(defproject org.clojars.vladimirmarkovic86/framework-lib "0.2.51"
   :description "Framework library"
   :url "https://github.com/VladimirMarkovic86/framework-lib"
   :license {:name "Eclipse Public License"
@@ -8,9 +8,9 @@
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.9"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.19"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.13"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.37"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.11"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.39"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.38"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.12"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.40"]
                  ]
 
   :min-lein-version "2.0.0"
